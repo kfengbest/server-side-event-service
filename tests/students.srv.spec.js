34e7d9c703091e94b307ed012be2b7318181cb89
Kai feng <kfeng@Kais-MacBook-Pro.local>
2021-11-26 15:58:39 -0800

    add more test cases

---
+++ tests/students.srv.spec.js
@@ -0,0 +1,13 @@
+const studentsSrv = require('../services/students.svc');
+
+describe('test student.service', () => {
+    it('should have students', () => {
+      let students = studentsSrv.getStudents();
+      expect(students).not.toBeNull;
+    });
+
+    it('should find students by id', () => {
+        let students = studentsSrv.getStudents("foo");
+        expect(students).not.toBeNull;
+      });
+  });
