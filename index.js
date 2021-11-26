824bd2b33564d4914c8e9b75279bc167b2f56a2e
Kai feng <kfeng@Kais-MacBook-Pro.local>
2021-11-26 11:17:04 -0800

    Basic app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('sse server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port => ${port}`);
+});
+
+module.exports = app;
